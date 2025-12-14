951d7baf39fa83bf1a7fa5e846780c01baa7b24f
fierogeneralalvarez-sys <fierogeneralalvarez@gmail.com>
2025-12-14 17:45:07 -0600

    Add files via upload

---
+++ Recursividad/Recursividad/MetodoRecursividad.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursividad
+{
+    internal class MetodoRecursividad
+    {
+        public int FactorialRecursividad(int factorial)
+        {
+            if (factorial == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return factorial * FactorialRecursividad(factorial - 1);
+            }
+            
+        }
+    }
+}
---
+++ Recursividad/Recursividad/Program.cs
@@ -0,0 +1,10 @@
+﻿using Recursividad;
+
+MetodoRecursividad mimetodo = new MetodoRecursividad();
+
+Console.WriteLine("¿A qué número le quieres sacar factorial?");
+int factorial = int.Parse(Console.ReadLine());
+
+int resultado = mimetodo.FactorialRecursividad(factorial);
+
+Console.WriteLine("El factorial es: " + resultado);
